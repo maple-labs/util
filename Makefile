9aa7f321d2208fffbd78f35786fc0c1ba2d1de8b
Lucas Manuel <lucas@maple.finance>
2021-08-12 14:33:46 -0400

    chore: Update `main` branch for consumption (#8)
    * feat: add config file
    
    * chore: Fix CI and modules, and add tests (SC-3086) (#1)
    
    * chore: Submodules and CI (SC-3086)
    
    * chore: Fix CI and modules, and add tests (SC-3086)
    
    * chore: PR fix
    
    * fix: globals paramater as address and CI tests (#3)
    
    * fix: Revert for library function selector (#5)
    
    * feat: Make Release (#6)
    
    * feat: Better Make Release (#7)
    
    Co-authored-by: Lucas Manuel <lucasmanuel@MacBook-Pro-2.local>
    Co-authored-by: Michael De Luca <35537333+deluca-mike@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-all    :; dapp build
-clean  :; dapp clean
-test   :; dapp test
-deploy :; dapp create Util
+prod    :; ./build.sh -c ./config/prod.json
+dev     :; ./build.sh -c ./config/dev.json
+clean   :; dapp clean
+test    :; ./test.sh
+release :; ./release.sh
--- contracts/Util.sol
+++ contracts/Util.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.6.11;
 
-import { SafeMath } from "../../../../lib/openzeppelin-contracts/contracts/math/SafeMath.sol";
+import { SafeMath } from "../modules/openzeppelin-contracts/contracts/math/SafeMath.sol";
 
-import { IERC20Details } from "../../../external-interfaces/IERC20Details.sol";
-
-import { IMapleGlobals } from "../../../core/globals/contracts/interfaces/IMapleGlobals.sol";
+import { IERC20DetailsLike } from "./interfaces/IERC20DetailsLike.sol";
+import { IMapleGlobals } from "./interfaces/IMapleGlobals.sol";
 
 /// @title Util is a library that contains utility functions.
 library Util {
@@ -23,10 +22,10 @@ library Util {
     function calcMinAmount(IMapleGlobals globals, address fromAsset, address toAsset, uint256 swapAmt) external view returns (uint256) {
         return
             swapAmt
-                .mul(globals.getLatestPrice(fromAsset))           // Convert from `fromAsset` value.
-                .mul(10 ** IERC20Details(toAsset).decimals())     // Convert to `toAsset` decimal precision.
-                .div(globals.getLatestPrice(toAsset))             // Convert to `toAsset` value.
-                .div(10 ** IERC20Details(fromAsset).decimals());  // Convert from `fromAsset` decimal precision.
+                .mul(globals.getLatestPrice(fromAsset))  // Convert from `fromAsset` value.
+                .mul(10 ** IERC20DetailsLike(toAsset).decimals())           // Convert to `toAsset` decimal precision.
+                .div(globals.getLatestPrice(toAsset))    // Convert to `toAsset` value.
+                .div(10 ** IERC20DetailsLike(fromAsset).decimals());        // Convert from `fromAsset` decimal precision.
     }
 
 }
